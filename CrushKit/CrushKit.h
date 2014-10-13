421c3dec1b9c0f6f4305ddbb6e03b8f61a7a8bd6
Aaron Freedman <aefreedman@nyu.edu>
2014-10-13 14:52:31 -0400

    Initial Commit

---
+++ CrushKit/CrushKit.h
@@ -0,0 +1,19 @@
+//
+//  CrushKit.h
+//  CrushKit
+//
+//  Created by Aaron E. Freedman on 10/13/14.
+//  Copyright (c) 2014 Secret Crush Corporation. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CrushKit.
+FOUNDATION_EXPORT double CrushKitVersionNumber;
+
+//! Project version string for CrushKit.
+FOUNDATION_EXPORT const unsigned char CrushKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CrushKit/PublicHeader.h>
+
+
